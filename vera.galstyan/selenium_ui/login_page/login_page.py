452eba6e93ab2efcf79cac5159b703a235b26161
ver-galstyan <vera.galstyan@inbox.ru>
2021-10-09 18:03:25 +0400

    vera.galstyan_Login_Page

---
+++ vera.galstyan/selenium_ui/login_page/login_page.py
@@ -0,0 +1,11 @@
+#This test case was created by Vera Galstyan
+
+#!/usr/bin/env python 3
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
+chrome_driver.get("https://www.saucedemo.com/inventory.html")
+chrome_driver.find_element(By.ID("user-name" ))
