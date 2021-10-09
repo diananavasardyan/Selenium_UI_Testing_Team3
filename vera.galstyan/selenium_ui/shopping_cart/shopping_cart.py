305f0dadb137714426b31542af062fc5ad7dc0d9
ver-galstyan <vera.galstyan@inbox.ru>
2021-10-09 19:13:11 +0400

    vera.galstyan_shopping_cart

---
+++ vera.galstyan/selenium_ui/shopping_cart/shopping_cart.py
@@ -0,0 +1,33 @@
+#This test case created by Vera Galstyan
+
+#!/usr/bin/env python 3
+
+#Shopping
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.common.by import By
+
+chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
+chrome_driver.get("https://www.saucedemo.com/inventory.html")
+chrome_driver.find_element(By.CLASS_NAME("btn btn_primary btn_small btn_inventory" ))
+
+#Continue shopping
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.common.by import By
+
+chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
+chrome_driver.get("https://www.saucedemo.com/inventory.html")
+chrome_driver.find_element(By.ID("first-name" ))
+
+
+chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
+chrome_driver.get("https://www.saucedemo.com/inventory.html")
+chrome_driver.find_element(By.ID("last-name" ))
+
+chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
+chrome_driver.get("https://www.saucedemo.com/inventory.html")
+chrome_driver.find_element(By.ID("ipostal-code" ))
