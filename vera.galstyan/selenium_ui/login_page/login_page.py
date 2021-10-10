b017fd1811d57de5b51210c355203e5cc92edd75
ver-galstyan <vera.galstyan@inbox.ru>
2021-10-10 12:16:03 +0400

    vera.galstyan_login_page Version 2

--- vera.galstyan/selenium_ui/login_page/login_page.py
+++ vera.galstyan/selenium_ui/login_page/login_page.py
@@ -8,4 +8,6 @@ from selenium.webdriver.common.by import By
 
 chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
 chrome_driver.get("https://www.saucedemo.com/inventory.html")
-chrome_driver.find_element(By.ID("user-name" ))
+chrome_driver.find_element(By.ID("user-name" )).click()
+chrome_driver.find_element(By.ID("password" )).click()
+chrome_driver.find_element(By.ID("login-button" )).click()
