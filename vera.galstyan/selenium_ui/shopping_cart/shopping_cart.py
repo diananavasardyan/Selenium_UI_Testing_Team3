e66aff9c4971fcb7a31a12c36a2c64ff333a948a
ver-galstyan <vera.galstyan@inbox.ru>
2021-10-17 23:04:26 +0400

    vera.galstyan_add_to_cartVersion 3

--- vera.galstyan/selenium_ui/shopping_cart/shopping_cart.py
+++ vera.galstyan/selenium_ui/shopping_cart/shopping_cart.py
@@ -2,32 +2,25 @@
 
 #!/usr/bin/env python 3
 
-#Shopping
+#This test case created by Vera Galstyan
+
+#!usr\bin\env python3
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
 from selenium.webdriver.common.by import By
-
-chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
-chrome_driver.get("https://www.saucedemo.com/inventory.html")
-chrome_driver.find_element(By.CLASS_NAME("btn btn_primary btn_small btn_inventory" ))
-
-#Continue shopping
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
-from selenium.webdriver.common.by import By
-
-chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
-chrome_driver.get("https://www.saucedemo.com/inventory.html")
-chrome_driver.find_element(By.ID("first-name"))
+chrome_options = Options()
 
 
-chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
-chrome_driver.get ("https://www.saucedemo.com/inventory.html")
-chrome_driver.find_element(By.ID("last-name"))
 
-chrome_driver = webdriver.Chrome("executable_path https://www.saucedemo.com/inventory.html")
-chrome_driver.get("https://www.saucedemo.com/inventory.html")
-chrome_driver.find_element(By.ID("ipostal-code" ))
+browser = webdriver.Chrome(executable_path = r"C:\Users\Admin\Desktop\Webdriver\chromedriver.exe", options = chrome_options)
+browser.get("https://www.saucedemo.com/")
+browser.find_element_by_id("user-name").send_keys("standard_user")
+browser.find_element_by_id("password").send_keys("secret_sauce")
+browser.find_element_by_id("login-button").click()
+actual = browser.find_element_by_id("add-to-cart-sauce-labs-backpack").send_keys("ADD TO CART")
+expect = "https://www.saucedemo.com/inventory.html"
+if  actual == expect:
+    print("Test case is passed.")
+else:
+    print("Test case is failed.")
