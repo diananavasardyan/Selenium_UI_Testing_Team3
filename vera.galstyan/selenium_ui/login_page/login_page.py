10dbdaf1547fd9559466a3dc0f2a1e9df74830d2
ver-galstyan <vera.galstyan@inbox.ru>
2021-10-24 12:31:07 +0400

    vera.galstyan_login_page Version 4

--- vera.galstyan/selenium_ui/login_page/login_page.py
+++ vera.galstyan/selenium_ui/login_page/login_page.py
@@ -8,11 +8,14 @@ from selenium.webdriver.common.by import By
 
 chrome_options = Options()
 
-
 browser = webdriver.Chrome(executable_path = r"C:\Users\Admin\Desktop\Webdriver\chromedriver.exe", options = chrome_options)
 browser.get("https://www.saucedemo.com/")
-browser.find_element_by_id("user-name").send_keys("standard_user")
-browser.find_element_by_id("password").send_keys("secret_sauce")
+username_form = username_form = browser.find_element_by_id("user-name")
+login = browser.find_element_by_xpath('//*[@id="login_credentials"]').text.split()[5]
+username_form.send_keys(login)
+password_form = browser.find_element_by_id("password")
+password = browser.find_element_by_xpath('//*[@class="login_password"]').text.split()[4]
+password_form.send_keys(password)
 browser.find_element_by_id("login-button").click()
 actual = browser
 expect = "https://www.saucedemo.com/inventory.html"
@@ -20,5 +23,4 @@ if  actual == expect:
     print("Test case is passed.")
 else:
     print("Test case is failed.")
-
 
