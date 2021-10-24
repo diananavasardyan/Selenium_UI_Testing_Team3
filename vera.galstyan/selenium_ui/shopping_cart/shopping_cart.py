b90ce92df77a377af66260d4d749b03230e10c6e
ver-galstyan <vera.galstyan@inbox.ru>
2021-10-24 12:37:08 +0400

    vera.galstyan_add_to_cart Version 5

--- vera.galstyan/selenium_ui/shopping_cart/shopping_cart.py
+++ vera.galstyan/selenium_ui/shopping_cart/shopping_cart.py
@@ -1,22 +1,24 @@
 #This test case created by Vera Galstyan
 
-
 #!usr\bin\env python3
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
 chrome_options = Options()
-
-
-
 browser = webdriver.Chrome(executable_path = r"C:\Users\Admin\Desktop\Webdriver\chromedriver.exe", options = chrome_options)
 browser.get("https://www.saucedemo.com/")
-browser.find_element_by_id("user-name").send_keys("standard_user")
-browser.find_element_by_id("password").send_keys("secret_sauce")
-browser.find_element_by_id("login-button").click()
+username_form = username_form = browser.find_element_by_id("user-name")
+login = browser.find_element_by_xpath('//*[@id="login_credentials"]').text.split()[5]
+username_form.send_keys(login)
+password_form = browser.find_element_by_id("password")
+password = browser.find_element_by_xpath('//*[@class="login_password"]').text.split()[4]
+password_form.send_keys(password)
+login_button = browser.find_element_by_id("login-button").click()
 actual = browser.find_element_by_id("add-to-cart-sauce-labs-backpack").send_keys("ADD TO CART")
 expect = "https://www.saucedemo.com/inventory.html"
+
 if  actual == expect:
     print("Test case is passed.")
 else:
